c58c66558054b67f6b30b574a1080722571bedda
Jose Cardoza <jos.cardoza@duocuc.cl>
2026-05-02 20:36:05 -0400

    implementando eureka

--- equipo/src/main/java/com/example/equipo/config/WebClientConfig.java
+++ equipo/src/main/java/com/example/equipo/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.equipo.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,9 +8,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
+    @LoadBalanced
     public WebClient webClient(){
         return WebClient.builder()
-                .baseUrl("http://localhost:8082/api/v1/clientes")
+                .baseUrl("http://cliente/api/v1/clientes")
                 .build();
     }
 }
