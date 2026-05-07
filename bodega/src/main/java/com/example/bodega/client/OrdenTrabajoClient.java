85951b4c3c1b6b47ddf25dc37906da0bb0b20516
Jose Cardoza <jos.cardoza@duocuc.cl>
2026-05-07 15:27:56 -0400

    solucionando errores en client

--- bodega/src/main/java/com/example/bodega/client/OrdenTrabajoClient.java
+++ bodega/src/main/java/com/example/bodega/client/OrdenTrabajoClient.java
@@ -1,18 +1,19 @@
 package com.example.bodega.client;
 
 import com.example.bodega.model.Orden_Trabajo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
-
+@Service
+@RequiredArgsConstructor
 public class OrdenTrabajoClient {
-    @Autowired
-    @Qualifier("Orden_TrabajoClient")
-    private WebClient webClient;
+    private final WebClient webClient;
 
     public Mono<Orden_Trabajo> obtenerOt(Long id, String token) {
         return webClient.get()
