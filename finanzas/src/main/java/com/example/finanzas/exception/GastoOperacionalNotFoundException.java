fd7a07f576dd3abb02b17b029c4667685625c2ea
rimsfar73 <ri.farias@duocuc.cl>
2026-05-10 18:49:26 -0400

    chore: limpiar diagnostico

--- finanzas/src/main/java/com/example/finanzas/exception/GastoOperacionalNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.finanzas.exception;
-
-public class GastoOperacionalNotFoundException extends RuntimeException {
-    public GastoOperacionalNotFoundException(Long id) {
-        super("Gasto operacional con id " + id + " no encontrado");
-    }
-}
