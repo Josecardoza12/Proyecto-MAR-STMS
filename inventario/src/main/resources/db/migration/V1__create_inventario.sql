61fdaab01716e62ec2f2b8d4c02996e61ae177b1
rimsfar73 <ri.farias@duocuc.cl>
2026-05-11 00:20:48 -0400

    fix: arreglando db.migration, eliminado clase de la entidad movimientorepuesto que se descartó

--- inventario/src/main/resources/db/migration/V1__create_inventario.sql
+++ inventario/src/main/resources/db/migration/V1__create_inventario.sql
@@ -6,14 +6,3 @@ CREATE TABLE IF NOT EXISTS repuesto (
     precio_sugerido DECIMAL(10,2),
     stock INT
 );
-
-CREATE TABLE IF NOT EXISTS movimiento_repuesto (
-    movimiento_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    repuesto_id BIGINT NOT NULL,
-    ot_id BIGINT NOT NULL,
-    tipo VARCHAR(10),
-    cantidad INT,
-    fecha DATE,
-    CONSTRAINT fk_mov_rep_repuesto
-        FOREIGN KEY (repuesto_id) REFERENCES repuesto(repuesto_id)
-);
